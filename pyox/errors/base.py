f97a810a50b156cf80c2c01c4187f2c41e3414c3
szwiletitsch <szwiletitsch@gmail.com>
2026-03-11 02:07:56 +0100

    fix(errors): change docstrings to match semantics of custom exception classes

--- pyox/errors/base.py
+++ pyox/errors/base.py
@@ -3,9 +3,9 @@ class ParseError(Exception):
     pass
 
 class LL1ConflictError(Exception):
-    """Base class for parser errors."""
+    """Base class for LL(1) parser conflict errors."""
     pass
 
 class SLR1ConflictError(Exception):
-    """Base class for parser errors."""
+    """Base class for SLR(1) parser conflict errors."""
     pass
